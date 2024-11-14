730c6daa5130915e7b9fe703204c5f21beffe4d9
BrahimMaarouf <bdaarkangelm@gmail.com>
2024-11-14 15:32:42 +0100

    main project for now

---
+++ RogueV1.0/phase3/help.py
@@ -0,0 +1,17 @@
+ 
+
+COMMANDS_HELP = {
+    "ls": "Usage: ls [-l] [-a]",
+    "cp": "Usage: cp [source] [destination]",
+    "mv": "Usage: mv [source] [destination]",
+    "rm": "Usage: rm [-r] [-f] [file/directory]",
+    "cd": "Usage: cd [path] or cd .. or cd -"
+}
+
+MANUALS = {
+    "ls": "ls - List directory contents. Options:\n -l: long format\n -a: show hidden files.",
+    "cp": "cp - Copy files or directories. Usage: cp [source] [destination]",
+    "mv": "mv - Move files or directories. Usage: mv [source] [destination]",
+    "rm": "rm - Remove files or directories. Usage: rm [-r] [-f] [file/directory]",
+    "cd": "cd - Change the current directory. Usage: cd [path] or cd .. or cd -"
+}
